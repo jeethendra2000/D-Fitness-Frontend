5cd658aa195b5c9827c500153fa63890e305082a
Jeethendra S R <Jeethendra@kerv.com>
2025-09-07 01:31:14 +0530

    Update teams, gallery, navbar with dynamic data with config folder and fixed bugs

---
+++ configs/navItemsList.ts
@@ -0,0 +1,10 @@
+// Navbar Items list
+export const navItemsList = [
+  { href: "/", label: "Home" },
+  { href: "/services", label: "Services" },
+  { href: "/team", label: "Our Team" },
+  { href: "/gallery", label: "Gallery" },
+  { href: "/contact", label: "Contact" },
+  { href: "/about", label: "About Us" },
+  { href: "/dashboard", label: "Dashboard" },
+];
---
+++ configs/teamMembersList.ts
@@ -0,0 +1,9 @@
+// Team members List
+export const TeamMembersList = [
+  { name: "Puneeth", role: "Owner", imageURL:"/img/team/team-1.jpg", socialLink:{facebook:'/', instagram:'/', whatsapp:'/', email:'/', youtube:'/'}},
+  { name: "Jeethendra", role: "Developer", imageURL:"/img/team/team-3.jpg", socialLink:{facebook:'/', instagram:'/', whatsapp:'/', email:'/', youtube:''}},
+  { name: "Jayanth Kumar", role: "Developer", imageURL:"/img/team/team-2.jpg", socialLink:{facebook:'/', instagram:'/', whatsapp:'', email:'/', youtube:''}},
+  { name: "Darshan", role: "Gym Trainer", imageURL:"/img/team/team-4.jpg", socialLink:{facebook:'/', instagram:'/', whatsapp:'', email:'', youtube:''}},
+  { name: "Likith Kumar", role: "Customer", imageURL:"/img/team/team-5.jpg", socialLink:{facebook:'/', instagram:'/', whatsapp:'', email:'', youtube:''}},
+  { name: "Vikas", role: "Customer", imageURL:"/img/team/team-6.jpg", socialLink:{facebook:'', instagram:'', whatsapp:'', email:'', youtube:''}},
+];
