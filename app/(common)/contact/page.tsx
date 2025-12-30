6a7d1471bd793b422fa44fb3f7fa57994a456857
Jeethendra S R <jeethendrajeethu8@gmail.com>
2025-12-31 01:53:31 +0530

    Added gallery, fixed enquiry form and update contact us

--- app/(common)/contact/page.tsx
+++ app/(common)/contact/page.tsx
@@ -1,5 +1,6 @@
 import GetInTouch from "@/components/homeComponents/getintouch";
 import Breadcrumb from "@/components/sections/breadcrumb";
+import ContactForm from "@/components/homeComponents/ContactForm"; // Import the new component
 import type { Metadata } from "next";
 
 export const metadata: Metadata = {
@@ -12,7 +13,7 @@ export default async function ContactUS() {
     <>
       <Breadcrumb title="Contact us" />
 
-      {/* <!-- Contact Section Begin --> */}
+      {/* */}
       <section className="contact-section spad">
         <div className="container">
           <div className="row">
@@ -34,43 +35,38 @@ export default async function ContactUS() {
                 <div className="cw-text">
                   <i className="fa fa-mobile"></i>
                   <ul>
-                    <li> +91 99016 47430</li>
-                    <li> +91 99016 47430</li>
+                    <li> +91 9108720358</li>
+                    <li> +91 8618964880</li>
                   </ul>
                 </div>
                 <div className="cw-text email">
                   <i className="fa fa-envelope"></i>
-                  <p>dfitness@gmail.com</p>
+                  <p>dfitnessgym2025@gmail.com</p>
                 </div>
               </div>
             </div>
 
             <div className="col-lg-6">
-              <div className="leave-comment">
-                <form action="#">
-                  <input type="text" placeholder="Name" />
-                  <input type="text" placeholder="Email" />
-                  <input type="text" placeholder="Website" />
-                  <textarea placeholder="Comment"></textarea>
-                  <button type="submit">Submit</button>
-                </form>
-              </div>
+              {/* Replaced static HTML form with the functional Client Component */}
+              <ContactForm />
             </div>
           </div>
 
           <div className="map">
+            {/* Updated Map Link to actually point to Hassan, Karnataka */}
             <iframe
-              src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d995151.6664747568!2d74.91060687812502!3d13.01584400000001!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3ba549574cd865ad%3A0x725c42ba1817d759!2sD%20FITNESS%20CENTER!5e0!3m2!1sen!2sin!4v1762932038892!5m2!1sen!2sin"
+              src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d62203.20818296232!2d76.05977937402035!3d13.006935105260192!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3ba5483296215033%3A0x2897c768c8b4b1a4!2sHassan%2C%20Karnataka!5e0!3m2!1sen!2sin!4v1709228471234!5m2!1sen!2sin"
               width="600"
               height="550"
               style={{ border: 0 }}
               allowFullScreen
               loading="lazy"
+              referrerPolicy="no-referrer-when-downgrade"
             ></iframe>
           </div>
         </div>
       </section>
-      {/* <!-- Contact Section End --> */}
+      {/* */}
 
       <GetInTouch />
     </>
