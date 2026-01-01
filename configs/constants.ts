e0f25ea084c1bcb98ee6ffebb6672c8ef800e244
dfitnessgym2025 <dfitnessgym2025@gmail.com>
2026-01-01 22:05:34 +0530

    Update constants.ts

--- configs/constants.ts
+++ configs/constants.ts
@@ -1,3 +1,3 @@
 // src/configs/constants.ts
-export const API_BASE_URL = "https://dfitnessgym.runasp.net/api";
+export const API_BASE_URL = "https://dfitnessgym2025.runasp.net/api";
 export const TOKEN = "YOUR_API_TOKEN_HERE";
